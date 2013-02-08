fe01867371a854105e22698c651e17d2e279d003
Sawai Hiroshi <info@info-town.jp>
2013-02-08 13:01:43 +0900

    Add top bottom option

--- popover.js
+++ popover.js
@@ -1,22 +1,21 @@
 /**
  * simple popup over jQuery plugin
  *
+ * @params pos position (top, bottom)
  */
-jQuery.fn.popover = function( type ) {
-    type = type || 'left';
+jQuery.fn.popover = function(pos) {
+    pos = pos || 'bottom';
     var selector = $(this.selector);
     var height = $(this).innerHeight();
     // pc
     if ($(window).width() >= 768) {
         // class popover HTMLElement
         $(selector).hover(function() {
-            var style = {
+            $('.pop', this).css(pos, height);
+            $('.pop', this).css({
                 position: 'absolute',
-                bottom: height,
                 left: 0
-            };
-            $('.pop', this).stop();
-            $('.pop', this).css(style);
+            });
             $('.pop', this).slideDown();
         }, function() {
             $('.pop', this).hide('slow');
